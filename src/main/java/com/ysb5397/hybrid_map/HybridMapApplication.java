31897838232c29464168d873796789c033462539
ysb5397 <ysb5397@gmail.com>
2025-09-30 14:40:17 +0900

    init

---
+++ src/main/java/com/ysb5397/hybrid_map/HybridMapApplication.java
@@ -0,0 +1,13 @@
+package com.ysb5397.hybrid_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HybridMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HybridMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ysb5397/hybrid_map/HybridMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysb5397.hybrid_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybridMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
